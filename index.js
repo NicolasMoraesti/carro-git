aa4cad71c912a8b977a5e7a0ae8edf89e3b49ba4
NicolasMoraesti <ncmoraees@gmail.com>
2025-05-19 16:08:49 -0300

    adiçao de input para modelo do veiculo

---
+++ index.js
@@ -0,0 +1,12 @@
+$($(document).ready(function() {
+  $('#bnt_adicionar').click(function(event) {
+    event.preventDefault();
+
+    var modelo = $('#modelo-veiculo')
+    var placa = $('#placa-veiculo')
+    var Marca = $('#marca-veiculo')
+    
+    alert(``);
+    });
+  })
+ )
